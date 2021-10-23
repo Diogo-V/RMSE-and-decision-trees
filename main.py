9d1e70202aaf7a78877c77e1e9e69dc64dd24f5b
Diogo Venâncio <diogo.venancio00@gmail.com>
2021-10-23 16:12:47 +0100

    Did more porgress on part 2

--- main.py
+++ main.py
@@ -2,11 +2,18 @@
 from scipy.io import arff
 import pandas as pd
 from sklearn.preprocessing import LabelBinarizer
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import StratifiedKFold
+from sklearn.feature_selection import SelectKBest, mutual_info_classif
+
+# Constants definition
+K = [1, 3, 5, 9]
+GROUP_NUMBER = 16  # Our group number
 
 # ----------------------------------------- PREPROCESSING AND DATA FILTERING ----------------------------------------- #
 
 # Loading dataset into working desk
-data = arff.loadarff('breast.w.arff')
+data = arff.loadarff('./data/breast.w.arff')
 df = pd.DataFrame(data[0])
 
 # Removes NaN values from dataset by deleting rows
@@ -25,3 +32,30 @@ for count, value in enumerate(y):
         y[count] = "no"
 lb = LabelBinarizer()
 y = lb.fit_transform(y)
+
+# Creates a k fold cross validator with 10 splits
+skf = StratifiedKFold(n_splits=10, shuffle=True, random_state=GROUP_NUMBER)
+
+# --------------------------------------------------- QUESTION 5.i --------------------------------------------------- #
+
+# Iterates over each number of selected features and creates a decision tree
+for k in K:
+
+    # Selects the best k features using mutual information
+    X_new = SelectKBest(mutual_info_classif, k=k).fit_transform(X, y.ravel())
+
+    # Creates tree with k max number of features and using information gain
+    tree = DecisionTreeClassifier(criterion="entropy", max_features=k)
+
+# --------------------------------------------------- QUESTION 5.ii -------------------------------------------------- #
+
+# Iterates over each number for maximum depth and creates a decision tree
+for k in K:
+
+    # Creates tree with k max depth
+    tree = DecisionTreeClassifier(max_depth=k)
+
+# ---------------------------------------------------- QUESTION 6 ---------------------------------------------------- #
+
+
+# ---------------------------------------------------- QUESTION 7 ---------------------------------------------------- #
